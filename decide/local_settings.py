a300b0dbf78faebdd3939bc8090bc98f4feebf56
Supremmaer <xavy14op@gmail.com>
2019-11-25 17:21:25 +0100

    h

--- decide/local_settings.py
+++ decide/local_settings.py
@@ -14,12 +14,12 @@ MODULES = [
 ]
 
 APIS = {
-    'authentication': 'http://127.0.0.1:8000',
-    'base': 'http://127.0.0.1:8000',
-    'booth': 'http://127.0.0.1:8000',
-    'census': 'http://127.0.0.1:8000',
-    'mixnet': 'http://127.0.0.1:8000',
-    'postproc': 'http://127.0.0.1:8000',
+    'authentication': 'http://decide-test12.herokuapp.com',
+    'base': 'http://decide-test12.herokuapp.com',
+    'booth': 'http://decide-test12.herokuapp.com',
+    'census': 'http://decide-test12.herokuapp.com',
+    'mixnet': 'http://decide-test12.herokuapp.com',
+    'postproc': 'http://decide-test12.herokuapp.com',
     'store': 'http://decide-test12.herokuapp.com',
     'visualizer': 'http://decide-test12.herokuapp.com',
     'voting': 'http://decide-test12.herokuapp.com',
